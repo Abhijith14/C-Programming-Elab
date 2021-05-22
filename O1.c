2502dafb0dee2b77d9ab61ce6d08fa8e3e85f19c
ABHIJITH UDAYAKUMAR <abhijithukzm@gmail.com>
2021-05-22 15:37:30 +0530

    Commit

---
+++ O1.c
@@ -0,0 +1,21 @@
+//. 2
+
+//O 1(nput & Ouput)
+
+//rite a program to read a character in upper case and then print it in lower case
+//Input and Output Format:
+//Refer sample input and output for formatting specification.
+//All float values are displayed correct to 2 decimal places.
+//All text in bold corresponds to input and the rest corresponds to output.
+/
+
+
+include <stdio.h>
+int main() 
+{
+	char a;
+  	scanf("%c",&a);
+  	a=tolower(a);
+  	printf("%c",a);
+	return 0;
+}
