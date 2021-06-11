30c0c6d912592d28311f076da1fa6ce7f0b89443
ABHIJITH UDAYAKUMAR <abhijithukzm@gmail.com>
2021-06-12 00:00:05 +0530

    Commit

---
+++ IO12.c
@@ -0,0 +1,20 @@
+//Q. 10:
+
+//IO 12(Input & Ouput )
+
+//Write a program to print the ASCII value of a character
+//Input and Output Format:
+//Refer sample input and output for formatting specification.
+//All float values are displayed correct to 2 decimal places.
+//All text in bold corresponds to input and the rest corresponds to output.
+//
+
+
+#include <stdio.h>
+int main()
+{
+	char n;
+  	scanf("%c",&n);
+  	printf("ASCII value of %c=%d",n,n);
+	return 0;
+}
