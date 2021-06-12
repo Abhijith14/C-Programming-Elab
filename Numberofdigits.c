a98abb8d3f60d105f158a65cd81ff2c893579c1b
ABHIJITH UDAYAKUMAR <abhijithukzm@gmail.com>
2021-06-13 00:00:06 +0530

    Commit

---
+++ Numberofdigits.c
@@ -0,0 +1,20 @@
+//Q. 12:
+
+//Number of digits(Data types )
+
+//C Program to Count number of digits in number without using mod operator
+//
+
+
+#include <stdio.h>
+#include <string.h>
+int main() 
+{
+	int num,l;
+  	char n[10];
+  	scanf("%d",&num);
+  	sprintf(n,"%d",num);
+  	l=strlen(n);
+  	printf("Number of Digits:%d",l);
+	return 0;
+}
