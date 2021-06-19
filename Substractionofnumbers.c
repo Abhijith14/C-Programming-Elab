3c7ed324e4b5997f7cea8c642779efc67bcd064b
ABHIJITH UDAYAKUMAR <abhijithukzm@gmail.com>
2021-06-20 00:17:03 +0530

    Commit

---
+++ Substractionofnumbers.c
@@ -0,0 +1,17 @@
+//Q. 20:
+
+//Substraction of numbers(Data types )
+
+//Harinis home work for second day is to subtract two numbers, help Harini to solve the problem.
+//
+
+
+#include <stdio.h>
+int main() 
+{
+	float a,b;
+  	scanf("%f%f",&a,&b);
+  	printf("The Subtraction of two number is:%f\n",a-b);
+  	printf("The Subtraction of two number is:%.2f\n",a-b);
+	return 0;
+}
