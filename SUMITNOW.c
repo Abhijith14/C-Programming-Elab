bb6bd2da38ac74fd238d4cbb0bd6ac5d619433ab
ABHIJITH UDAYAKUMAR <abhijithukzm@gmail.com>
2021-06-23 00:00:17 +0530

    Commit

---
+++ SUMITNOW.c
@@ -0,0 +1,22 @@
+//Q. 23:
+
+//SUM IT NOW(Operators )
+
+//Nilo and Teena are playing with each other. Nilo challenges Teena to sum the digit of a number. Help Teena to answer as quick as possible.
+//
+
+
+#include <stdio.h>
+int main() 
+{
+	int n,s=0,rem;
+  	scanf("%d",&n);
+    while(n!=0)
+    {
+      rem=n%10;
+      s=s+rem;
+      n=n/10;
+    }
+    printf("%d",s);
+    return 0;
+}
