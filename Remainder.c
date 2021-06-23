83325abce7380b2da1706ff111ca3d7f7b72ca81
ABHIJITH UDAYAKUMAR <abhijithukzm@gmail.com>
2021-06-24 00:00:05 +0530

    Commit

---
+++ Remainder.c
@@ -0,0 +1,20 @@
+//Q. 24:
+
+//Remainder(Operators )
+
+//Write a program to find the remainder when two given numbers are divided. he first line contains an integer T, total number of test cases. Then follow T lines, each line contains two Integers A and B. Find remainder when A is divided by B
+//
+
+
+#include <stdio.h>
+int main() 
+{
+	int T,i,A,B;
+    scanf("%d",&T);
+    for(i=0;i<T;i++)
+    {
+    	scanf("%d%d",&A,&B);
+        printf("%d\n",A%B);
+    }
+	return 0;
+}
