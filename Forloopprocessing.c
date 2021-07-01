1743ebb91a3bb605eeea4dc279c935382e242cea
ABHIJITH UDAYAKUMAR <abhijithukzm@gmail.com>
2021-07-01 06:10:15 +0530

    Commit

---
+++ Forloopprocessing.c
@@ -0,0 +1,20 @@
+//Q. 31:
+
+//For loop processing(Control and Looping )
+
+//Program to calculate the sum of first n natural numbers
+//
+
+
+#include <stdio.h>
+int main()
+{
+  	int N,s=0,i;
+  	scanf("%d",&N);
+	for(i=1;i<=N;i++)
+    {
+      s=s+i;
+    }
+  	printf("%d",s);
+	return 0;
+}
