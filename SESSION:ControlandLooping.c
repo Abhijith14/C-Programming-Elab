734d6a9eb70bb8ecbd3636592f38aa3814a624dd
ABHIJITH UDAYAKUMAR <abhijithukzm@gmail.com>
2021-07-03 00:00:10 +0530

    Commit

---
+++ SESSION:ControlandLooping.c
@@ -0,0 +1,26 @@
+//Q. 33:
+
+//SESSION: Control and Looping( )
+
+//Write a program to generate a following #s triangle:
+//# # # # #
+//# # # #
+//# # #
+//# #
+//#
+//
+
+
+#include <stdio.h>
+int main() 
+{
+	int n,i,j;
+  	scanf("%d",&n);
+  	for(i=0;i<n;i++)
+    {
+      for(j=n;j>i;j--)
+        printf("#");
+      printf("\n");
+    }
+	return 0;
+}
