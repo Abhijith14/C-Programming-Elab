1254b9f908b740949e196f88efc02edd23240555
ABHIJITH UDAYAKUMAR <abhijithukzm@gmail.com>
2021-07-07 00:00:07 +0530

    Commit

---
+++ BINARYADDANDBINARYSUB.c
@@ -0,0 +1,16 @@
+//Q. 37:
+
+//BINARY ADD AND BINARY SUB(Control and Looping )
+
+//Jonnes gave smith two numbers and asked him to find Binary Addition and Binary Subtraction.Write a C code to help smith to find Binary Addition and Binary Subtraction for a given number
+//
+
+
+#include <stdio.h>
+int main() 
+{
+	int a,b;
+  	scanf("%d%d",&a,&b);
+  	printf("%d\n%d",a+b,a-b);
+	return 0;
+}
