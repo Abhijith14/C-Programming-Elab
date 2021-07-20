08e0b51760240e3456363b352050159d725e0620
ABHIJITH UDAYAKUMAR <abhijithukzm@gmail.com>
2021-07-21 00:00:09 +0530

    Commit

---
+++ SumofpositiveNumbers.c
@@ -0,0 +1,27 @@
+//Q. 49:
+
+//Sum of positive Numbers(Arrays )
+
+//Write a program to find the sum of positive numbers in an array.
+//Input Format:
+//Input consists of n+1 integers. The first integer corresponds to n , the size of the array. The next ?n? integers correspond to the elements in the array. Assume that the maximum value of n is 15.
+//Output Format:
+//Refer sample output for details
+//
+
+
+#include <stdio.h>
+int main()
+{
+	int s=0,n,Ar[30],i;
+  	scanf("%d",&n);
+  	for(i=0;i<n;i++)
+      scanf("%d",&Ar[i]);
+  	for(i=0;i<n;i++)
+    {
+      if(Ar[i]>0)
+        s=s+Ar[i];
+    }
+  	printf("sum=%d",s);
+	return 0;
+}
