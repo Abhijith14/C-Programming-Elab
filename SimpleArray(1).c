a8209cfdae435c689129fe6b08b61f36c5521fb9
ABHIJITH UDAYAKUMAR <abhijithukzm@gmail.com>
2021-07-22 00:00:05 +0530

    Commit

---
+++ SimpleArray(1).c
@@ -0,0 +1,24 @@
+//Q. 50:
+
+//Simple Array(Arrays )
+
+//Calculate average of n numbers
+//
+
+
+#include <stdio.h>
+int main()
+{
+	int i,l,s=0,avg;
+    int Ar[10];
+  	scanf("%d",&l);
+  	for(i=0;i<l;i++)
+      scanf("%d",&Ar[i]);
+  	for(i=0;i<l;i++)
+    {
+      s=s+(int)Ar[i];
+    }
+  	avg=s/l;
+  	printf("%d",avg);
+	return 0;
+}
