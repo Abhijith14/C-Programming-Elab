cdc3a47fd655e5e0e06cf8e1e99794a13202e9c4
ABHIJITH UDAYAKUMAR <abhijithukzm@gmail.com>
2021-08-05 00:00:06 +0530

    Commit

---
+++ POWEROF2.c
@@ -0,0 +1,24 @@
+//Q. 65:
+
+//POWER OF 2(Functions )
+
+//Ram challenges Ganga to write a code to check whether an Integer Number is power of two (2) or not. Can you help Ganga to come out of this problem.
+//
+
+
+#include <stdio.h>
+#include <math.h>
+void Test(int n)
+{
+ if(ceil(log2(n))==floor(log2(n)))
+    printf("YES");
+ else
+    printf("NO");
+}
+int main() 
+{
+	int n;
+  	scanf("%d",&n);
+  	Test(n);
+	return 0;
+}
