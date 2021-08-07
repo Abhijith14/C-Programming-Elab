c80363d4debf60bfe75a2afba16d1123a76f5124
ABHIJITH UDAYAKUMAR <abhijithukzm@gmail.com>
2021-08-08 00:00:08 +0530

    Commit

---
+++ RecursiveFunctions.c
@@ -0,0 +1,23 @@
+//Q. 69:
+
+//Recursive Functions(Functions )
+
+//Write a C program for Sum of Natural Numbers Using Recursion.
+//
+
+
+#include <stdio.h>
+int Numbers(int n) 
+{
+   if(n!=0)
+     return (n+Numbers(n-1));
+   else
+     return n;
+}
+int main() 
+{
+    int num;
+    scanf("%d", &num);
+    printf("%d",Numbers(num));
+    return 0;
+}
