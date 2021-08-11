79d81b2fa12e1bac86325f3785cc6ad05a72d8f0
ABHIJITH UDAYAKUMAR <abhijithukzm@gmail.com>
2021-08-12 00:00:08 +0530

    Commit

---
+++ THREEMARKS.c
@@ -0,0 +1,27 @@
+//Q. 73:
+
+//THREE MARKS(Structures and Unions )
+
+//Mani Working as a professor in ABC college, have
+//to get students three subjects points.
+//so he planned to do one program to implement
+//structure concept.
+//Input
+//3 3 5
+//Output
+//3 3 5
+//
+
+
+#include <stdio.h>
+struct Mark
+{
+  int a,b,c;
+};
+int main()
+{
+	struct Mark M;
+  	scanf("%d%d%d",&M.a,&M.b,&M.c);
+  	printf("%d\n%d\n%d",M.a,M.b,M.c);
+	return 0;
+}
