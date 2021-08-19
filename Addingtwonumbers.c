692328e06d49ba589889c34050b5302c11ef0c9b
ABHIJITH UDAYAKUMAR <abhijithukzm@gmail.com>
2021-08-20 00:00:09 +0530

    Commit

---
+++ Addingtwonumbers.c
@@ -0,0 +1,20 @@
+//Q. 81:
+
+//Adding two numbers(Pointers )
+
+//Add two numbers using pointers
+//
+
+
+#include <stdio.h>
+int main() 
+{
+  	int n1,n2;
+	int* a;
+  	int* b;
+  	scanf("%d%d",&n1,&n2);
+  	a=&n1;
+  	b=&n2;
+  	printf("The sum of the entered numbers is=%d",*a+*b);
+	return 0;
+}
