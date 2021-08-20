32b4601b96738a6e03cd3407afa0c7b8af1407da
ABHIJITH UDAYAKUMAR <abhijithukzm@gmail.com>
2021-08-21 00:00:05 +0530

    Commit

---
+++ REVARRAY.c
@@ -0,0 +1,28 @@
+//Q. 82:
+
+//REV ARRAY(Pointers )
+
+//program using pointers to read in an array of integers and print its elements in reverse order
+//
+
+
+#include <stdio.h>
+int main() 
+{
+	int i,n;
+  	int Ar[30],*p;
+    p=&Ar[0];
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+      scanf("%d",p);
+      p++;
+    }
+    p=&Ar[n-1];
+    for(i=n-1;i>=0;i--)
+    {
+      printf("%d ",*p);
+      p--;
+    }
+	return 0;
+}
