b8826e1fc51c44ae2cc6a5d3eb085eec2484e58d
ABHIJITH UDAYAKUMAR <abhijithukzm@gmail.com>
2021-08-27 00:00:08 +0530

    Commit

---
+++ BIGGESTVALUEINAGIVENARRAY.c
@@ -0,0 +1,25 @@
+//Q. 88:
+
+//BIGGEST VALUE IN A GIVEN ARRAY(Pointers )
+
+//Find biggest value in the array using pointers in C
+//
+
+
+#include <stdio.h>
+#include <string.h>
+int main() 
+{
+	int *p,n,i,Ar[10];
+  	scanf("%d",&n);
+  	for(i=0;i<n;i++)
+      scanf("%d",&Ar[i]);
+  	p=&Ar[0];
+  	for(i=0;i<n;i++)
+    {
+      if(Ar[i]>*p)
+        *p=Ar[i];
+    }
+  	printf("%d",*p);
+  	return 0;
+}
