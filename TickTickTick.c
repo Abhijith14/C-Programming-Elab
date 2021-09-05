346b532571125a0ee21a0ceada740c088e628a7f
ABHIJITH UDAYAKUMAR <abhijithukzm@gmail.com>
2021-09-06 00:00:09 +0530

    Commit

---
+++ TickTickTick.c
@@ -0,0 +1,16 @@
+//Q. 98:
+
+//Tick Tick Tick(Practice Session )
+
+//Hemas teacher give one assignment to him. she should write a code to find the time entered in hours,seconds and minutes into seconds. Help him to solve this task
+//
+
+
+#include <stdio.h>
+int main() 
+{
+	int h,m,s;
+    scanf("%d%d%d",&h,&m,&s);
+    printf("%d",(h*3600)+(m*60)+s);
+	return 0;
+}
